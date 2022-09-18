317f459e9b50b0b6b7d36e20ab17d716c458023f
alex_ss <sixbeysa@gmail.com>
2022-09-17 21:19:55 -0700

    Add functions to display word and guessed letters, add function to check if the player won

--- js/script.js
+++ js/script.js
@@ -22,7 +22,7 @@ const guessedLetters = [];
 
 
 // Display circle symbols as placeholders for the chosen word's letters
-const progressUpdate = function (word) {
+const placeholder = function (word) {
     const wordLength = [];
     for (const letter of word) {
       console.log(letter);
@@ -30,7 +30,7 @@ const progressUpdate = function (word) {
     }
     wordInProgress.innerText = wordLength.join("");
   };
-progressUpdate(word);
+placeholder(word);
 
 //guess button click event to capture input and then clear input field
 buttonGuess.addEventListener("click", function (e) {
@@ -66,5 +66,40 @@ const makeGuess = function (letter) {
     } else {
         guessedLetters.push(letter);
         console.log(guessedLetters);
+        showGuesses();
+        progressUpdate(guessedLetters);
     }
+};
+
+const showGuesses = function () {
+    guessedLettersDisplay.innerHTML = "";
+    for (const letter of guessedLetters) {
+        const li = document.createElement("li");
+        li.innerText = letter;
+        guessedLettersDisplay.append(li);
+    }
+};
+
+const progressUpdate = function (guessedLetters) {
+    const wordUpper = word.toUpperCase();
+    const wordArray = wordUpper.split("");
+    console.log(wordArray);
+    const revealWord = [];
+    for (const letter of wordArray) {
+      if (guessedLetters.includes(letter)) {
+        revealWord.push(letter.toUpperCase());
+      } else {
+        revealWord.push("●");
+      }
+    }
+    // console.log(revealWord);
+    wordInProgress.innerText = revealWord.join("");
+    successfulGuess();
+};
+
+const successfulGuess = function () {
+    if (word.toUpperCase() === wordInProgress.innerText) {
+        message.classList.add("win");
+        message.innerHTML = `<p class="highlight">You guessed the correct word! Congrats!</p>`;
+    } 
 };
